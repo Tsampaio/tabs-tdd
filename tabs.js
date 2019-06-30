f7e7e0c7942cda8539309ee0bf121961799db0a9
Telmo Sampaio <telmoasampaio@gmail.com>
2019-06-30 22:35:48 +0100

    adding JS file and missing css classes to html

--- tabs.js
+++ tabs.js
@@ -1,1 +1,19 @@
-console.log('log from tabs.js');
+const allAnchor = document.getElementsByClassName("tabs__anchor");
+const allContent = document.querySelectorAll(".tabs__content");
+
+Array.from(allAnchor).map((anchor, i) => {
+
+    anchor.addEventListener("click",(event) => { 
+        Array.from(allAnchor).map((anchor) => {
+            anchor.classList.remove("tabs__anchor-active");
+        });
+
+        Array.from(allContent).map((content) => {
+            content.classList.remove("tabs__content-active");
+        });
+
+        event.target.classList.add("tabs__anchor-active");
+        allContent[i].classList.add("tabs__content-active");
+    });
+
+});
