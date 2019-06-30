ca92930d01adfd1e22671e8e751dbe3b1d7ca462
Telmo Sampaio <telmoasampaio@gmail.com>
2019-06-30 22:11:57 +0100

    test code refactor

--- tabs.test.js
+++ tabs.test.js
@@ -1,42 +1,16 @@
-test('when clicking a tab makes it active', () => {
-
-    document.body.innerHTML = 
+function tabs() {
+    return (
         `<ul class="tabs__menu">
             <li class="tabs__item"><a class="tab1 tabs__anchor tabs__anchor-active" href="#">Terminal 1</a></li>
             <li class="tabs__item"><a class="tab2 tabs__anchor" href="#">Terminal 2</a></li>
             <li class="tabs__item"><a class="tab3 tabs__anchor" href="#">Terminal 3</a></li>
-        </ul>`;
+        </ul>`
+    );
+}
 
-        const allAnchor = document.getElementsByClassName("tabs__anchor");
-
-        Array.from(allAnchor).map((anchor, i) => {
-
-            anchor.addEventListener("click",(event) => { 
-                Array.from(allAnchor).map((anchor) => {
-                    anchor.classList.remove("tabs__anchor-active");
-                });
-        
-                event.target.classList.add("tabs__anchor-active");
-            });
-        
-        });
-            
-        document.querySelector('.tab3').click();
-
-        expect(document.querySelector('.tab3').classList).toContain('tabs__anchor-active');
-        expect(document.querySelector('.tab1').classList).not.toContain('tabs__anchor-active');
-        expect(document.querySelector('.tab2').classList).not.toContain('tabs__anchor-active');
-});
-
-test('when clicking a tab makes content display', () => {
-
-    document.body.innerHTML = 
-        `<ul class="tabs__menu">
-            <li class="tabs__item"><a class="tab1 tabs__anchor tabs__anchor-active" href="#">Terminal 1</a></li>
-            <li class="tabs__item"><a class="tab2 tabs__anchor" href="#">Terminal 2</a></li>
-            <li class="tabs__item"><a class="tab3 tabs__anchor" href="#">Terminal 3</a></li>
-        </ul>
-        <div class="container">
+function content() {
+    return (
+        `<div class="container">
             <div class="content1 tabs__content tabs__content-active">
                 <h1>content 1</h1>
             </div>
@@ -46,9 +20,11 @@ test('when clicking a tab makes content display', () => {
             <div class="content3 tabs__content">
                 <h1>content 3</h1>
             </div>
-        </div>`;
+        </div>`
+    );
+}
 
-
+function activeTab() {
     const allAnchor = document.getElementsByClassName("tabs__anchor");
     const allContent = document.querySelectorAll(".tabs__content");
 
@@ -66,7 +42,35 @@ test('when clicking a tab makes content display', () => {
             allContent[i].classList.add("tabs__content-active");
         });
     });
+}
 
+test('when clicking a tab makes it active', () => {
+
+    document.body.innerHTML = 
+        `<div class="tabs">
+            ${tabs()}
+            ${content()}
+        </div>`;
+
+        activeTab();
+            
+        document.querySelector('.tab3').click();
+
+        expect(document.querySelector('.tab3').classList).toContain('tabs__anchor-active');
+        expect(document.querySelector('.tab1').classList).not.toContain('tabs__anchor-active');
+        expect(document.querySelector('.tab2').classList).not.toContain('tabs__anchor-active');
+});
+
+test('when clicking a tab makes content display', () => {
+
+    document.body.innerHTML = 
+        `<div class="tabs">
+            ${tabs()}
+            ${content()}
+        </div>`;
+
+    activeTab();
+    
     document.querySelector('.tab3').click();
 
     expect(document.querySelector('.content3').classList).toContain('tabs__content-active');
@@ -76,21 +80,9 @@ test('when clicking a tab makes content display', () => {
 
 test('should render correctly on all devices', () => {
     const  myContent = 
-        `<ul class="tabs__menu">
-            <li class="tabs__item"><a class="tab1 tabs__anchor tabs__anchor-active" href="#">Terminal 1</a></li>
-            <li class="tabs__item"><a class="tab2 tabs__anchor" href="#">Terminal 2</a></li>
-            <li class="tabs__item"><a class="tab3 tabs__anchor" href="#">Terminal 3</a></li>
-        </ul>
-        <div class="container">
-            <div class="content1 tabs__content tabs__content-active">
-                <h1>content 1</h1>
-            </div>
-            <div class="content2 tabs__content">
-                <h1>content 2</h1>
-            </div>
-            <div class="content3 tabs__content">
-                <h1>content 3</h1>
-            </div>
+        `<div class="tabs">
+            ${tabs()}
+            ${content()}
         </div>`;
 
         // const renderer = require('react-test-renderer'); missing react?!?!?
